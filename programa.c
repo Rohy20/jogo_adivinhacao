131f9810fbfc192af78ff4ad0ece151fab9b634e
rohy20 <rohymaia@outlook.com>
2024-09-26 01:11:09 -0300

    Second Commit

--- programa.c
+++ programa.c
@@ -1,72 +1,113 @@
+// including the libraries
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
 
-//Definindo numero de tentativas
-#define Try 5 //constante
+int main() {
 
-int main(){
-    //cabecalho do jogo
-    printf("*****************************\n");
-    printf("*Welcome to my Guessing Game*\n");
-    printf("*****************************\n");
+    // printing a nice game header
+    printf("\n\n");
+    printf("          P  /_\\  P                              \n");
+    printf("         /_\\_|_|_/_\\                            \n");
+    printf("     n_n | ||. .|| | n_n         Welcome to the   \n");
+    printf("     |_|_|nnnn nnnn|_|_|         Guessing Game!   \n");
+    printf("    |\" \"  |  |_|  |\"  \" |                     \n");
+    printf("    |_____| ' _ ' |_____|                         \n");
+    printf("          \\__|_|__/                              \n");
+    printf("\n\n");
 
-    //numero secreto randomico
-    int seconds = time(0); //Mudando a semente da f rand() para garantir que rand() gere um numero realmente aleatorio.
-    srand(seconds);
-    int bignum = rand(); //funcao que trara numeros randomicos, porem muito grandes
-    int secretnumber = bignum % 100; //aqui pegamos o resto da divisao por 100, para garantir que o numero sera sempre entre 0 e 99.
-
-
-    //chute do jogador
+    // declaring variables that will be used later
     int guess;
-    //int win = 0;
-    //int try = 1;
+    int correct;
+    int level;
+    int total_attempts;
 
-    int points = 1000;
+    // defining the initial score
+    double score = 1000;
 
-    //perguntando ao jogador qual o chute, e imprimindo na tela
-    for(int i = 1; i <= Try; i++){ //loop de de acordo com as tentativas
-    //while(win == 0){ //loop indeterminado, ate o jogador acertar
-    //printf("Try %i\n", try + 1);
-    printf("Try %i of %i\n", i, Try);
-    printf("What's your guess?? ");
-    scanf("%i", &guess);
-    printf("Your guess is %i \n", guess);
+    // generating a random secret number
+    srand(time(0));
+    int secret_number = rand() % 100;
 
-    //Numero ou caracter invalido
-    if(guess < 0){
-        printf("Invalid Guess \n");
-        //i--;
+    printf("%d", secret_number);
 
-        continue;
+    // choosing the difficulty level
+    printf("What is the difficulty level?\n");
+    printf("(1) Easy (2) Medium (3) Hard\n\n");
+    printf("Choose: ");
+
+    scanf("%d", &level);
+
+    switch(level) {
+        case 1: 
+            total_attempts = 20;
+            break;
+        case 2:
+            total_attempts = 15;
+            break;
+        default:
+            total_attempts = 6;
+            break;
     }
-    
 
-    //condicionais para caso o jogador acerte ou erre.
-    int correct = guess == secretnumber;
-    int Higher = guess < secretnumber;
-    int Lower = guess > secretnumber;
+    // main game loop
+    for(int i = 1; i <= total_attempts; i++) {
 
-    if(correct){
-        printf("Nice! You did'it\n");
-        printf("You did %d\n", points);
-        //para de executar
-        break;
-    }
-    else{
-       if(Lower){
-        points -= guess;
-        printf("Lower than that\n");
-        
-       }
-       if(Higher){
-        points -= guess;
-            printf("Higher than that\n");
+        printf("-> Attempt %d of %d\n", i, total_attempts);
+
+        printf("Guess a number: ");
+        scanf("%d", &guess);
+
+        // handling negative number guesses
+        if(guess < 0) {
+            printf("You can't guess negative numbers\n");
+            i--;
+            continue;
         }
+
+        // checks if the guess is correct, higher, or lower
+        correct = guess == secret_number;
+
+        if(correct) {
+            break;
+        } else if(guess > secret_number) {
+            printf("\nLower than that!\n\n");
+        } else {
+            printf("\nhigher than that!\n\n");
+        }
+
+        // calculates the score loss
+        double points_lost = abs(guess - secret_number) / 2.0;
+        score = score - points_lost;
     }
-    //try++; //toda vez que o codigo for executado, ele contara mais uma tentativa do jogador, ate que ele acerte
+
+    // printing victory or defeat message
+    printf("\n");
+    if(correct) {
+        printf("             OOOOOOOOOOO               \n");
+        printf("         OOOOOOOOOOOOOOOOOOO           \n");
+        printf("      OOOOOO  OOOOOOOOO  OOOOOO        \n");
+        printf("    OOOOOO      OOOOO      OOOOOO      \n");
+        printf("  OOOOOOOO  #   OOOOO  #   OOOOOOOO    \n");
+        printf(" OOOOOOOOOO    OOOOOOO    OOOOOOOOOO   \n");
+        printf("OOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOO  \n");
+        printf("OOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOO  \n");
+        printf("OOOO  OOOOOOOOOOOOOOOOOOOOOOOOO  OOOO  \n");
+        printf(" OOOO  OOOOOOOOOOOOOOOOOOOOOOO  OOOO   \n");
+        printf("  OOOO   OOOOOOOOOOOOOOOOOOOO  OOOO    \n");
+        printf("    OOOOO   OOOOOOOOOOOOOOO   OOOO     \n");
+        printf("      OOOOOO   OOOOOOOOO   OOOOOO      \n");
+        printf("         OOOOOO         OOOOOO         \n");
+        printf("             OOOOOOOOOOOO              \n");
+        printf("\nCongratulations! You got it right!\n");
+        printf("You scored %.2f points. See you next time!\n\n", score);
+    } else {
+
+        printf("       \\|/ ____ \\|/    \n");   
+        printf("        @~/ ,. \\~@      \n");   
+        printf("       /_( \\__/ )_\\    \n");   
+        printf("          \\__U_/        \n");
+
+        printf("\nYou lost! Try again!\n\n");
     }
-    
-return 0;
-};
+}
